61b11c868962a81ce24af83566b96a26ff46f531
Dominic <dominicdellasera@gmail.com>
2022-12-16 13:48:03 -0500

    Fix Readme.md

--- Readme.md
+++ Readme.md
@@ -1,8 +1,8 @@
-#About
+# About
 
 This is a following along of the micrograd tutorial. I'm doing this small project in order to explore the class features of Python and get a better feel for how Neural Networks work. 
 
-#Testing
+# Testing
 I have started out with some basic unit tests on Python's features, and will adopt a testing framework and clean this up later.
 
 https://www.youtube.com/watch?v=VMj-3S1tku0
--- classPractice.py
+++ classPractice.py
@@ -1,19 +1,23 @@
+from graphviz import Digraph
+
 class Value:
     #Self is roughly eqivalant to this, 
     #__init__ is the constructor
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
     #repr is the toString override
     def __repr__(self):
     #Is this constructing a new object every time?
         return f"Value(data={self.data})"
         
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
         return out
         
     def __mul__(self, other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
         return out
         
    
@@ -39,17 +43,58 @@ class Value:
 #f′(z)=ux+ivx=vy−iuy.−iuy.
         print("ConjTrans", self.toComplex().imag)
         #return toComplex(-self.)
+    
+def trace(root):
+    #Builds a set of all nodes and edges in a graphviz
+    nodes, edges = set(), set()
+    
+
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+ 
+    
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir' : 'LR'}) ##LR = left to right
+    
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
         
+        #for any value in the graph, create a rectangular ('record') node for items
+        dot.node(name = uid, label = "{ data %.4f }" % ( n.data, ), shape='record')
+        #and connect this node to items
+        dot.edge(uid + n._op, uid)
+        
+        for n1, n2 in edges:
+       #Connect n1 to the op node of n2
+            dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    return dot
+       
+       
+       
         
 a = Value(2.0)
 
 b = Value(-3.0)
 
+c = Value(10.0)
+
+d = a*b + c
 #a.setOtherData(5)
 #a.getOtherData
 #print(a.getOtherData())
 #print(a.toComplex())
 #print(a.toConjugateTranspose())
 
-print(a+b)
-print(a*b)
+
+print(d)
+print(d._prev)
+print(d._op)
+
+draw_dot(d)
